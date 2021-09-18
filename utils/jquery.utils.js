6f678fd9bb82d9e90287a0560fc499847b887878
crackxlash <duartedeo@hotmail.com>
2021-09-18 12:52:31 -0500

    Update jquery.utils.js
    Add MakeTables

--- utils/jquery.utils.js
+++ utils/jquery.utils.js
@@ -220,3 +220,38 @@ Date.prototype.format = function (format, separator) {
             break;
     }
 };
+
+// Builds the HTML Table out of myList.
+var buildHtmlTable = function(selector) {
+    var columns = addAllColumnHeaders(myList, selector);
+
+    for (var i = 0; i < myList.length; i++) {
+        var row$ = $('<tr/>');
+        for (var colIndex = 0; colIndex < columns.length; colIndex++) {
+            var cellValue = myList[i][columns[colIndex]];
+            if (cellValue == null) cellValue = "";
+            row$.append($('<td/>').html(cellValue));
+        }
+        $(selector).append(row$);
+    }
+}
+// Adds a header row to the table and returns the set of columns.
+// Need to do union of keys from all records as some records may not contain
+// all records.
+var addAllColumnHeaders = function(data, selector) {
+    var columnSet = [];
+    var headerTr$ = $('<tr/>');
+
+    for (var i = 0; i < myList.length; i++) {
+        var rowHash = myList[i];
+        for (var key in rowHash) {
+            if ($.inArray(key, columnSet) == -1) {
+                columnSet.push(key);
+                headerTr$.append($('<th/>').html(key));
+            }
+        }
+    }
+    $(selector).append(headerTr$);
+
+    return columnSet;
+}

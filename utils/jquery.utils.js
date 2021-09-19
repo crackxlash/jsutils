cd0d90628925e7cab75313cf017639a42544ca84
crackxlash <duartedeo@hotmail.com>
2021-09-19 18:37:30 -0500

    AddModalOptions
    V.3123

--- utils/jquery.utils.js
+++ utils/jquery.utils.js
@@ -163,6 +163,34 @@ $.fn.fill = function (optionDefault, key, value, data) {
     }
 };
 
+$.fn.loadData = function (params) {
+    if (params.buttonOK !== undefined) {
+        var attr = params.buttonOK.options === undefined ? null : params.buttonOK.options;
+        var $btnSave = $('<button/>')
+            .addClass('psld btn btn-' + params.buttonOK.class)
+            .html(params.buttonOK.text);
+        $btnSave.attr('onclick', params.buttonOK.method);
+        if (attr != null) $btnSave.attr(attr);
+    }
+
+    $(this).removeClass('bd-example-modal-sm');
+    $(this).find('.modal-dialog').removeClass('modal-sm');
+    $(this).removeClass('bd-example-modal-lg');
+    $(this).find('.modal-dialog').removeClass('modal-lg');
+
+    if (params.size !== undefined) {
+        $(this).addClass(params.size.contentCls);
+        $(this).find('.modal-dialog').addClass(params.size.bodyCls);
+    }
+
+    $(this).find('.modal-title').empty().html(params.title);
+    $(this).find('.modal-body').empty().html(params.body);
+    $(this).find('.modal-footer').find('.psld').remove();
+    $(this).find('.modal-footer').append($btnSave);
+    $(this).find('.modal-footer').removeClass('d-none');
+    if (!params.visibleFooter) $(this).find('.modal-footer').addClass('d-none');
+}
+
 String.prototype.getExtension = function () {
     var re = /(?:\.([^.]+))?$/;
     return re.exec(this)[1];

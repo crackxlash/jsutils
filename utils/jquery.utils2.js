19f0c83b8a31ebc47d5344825d0d14a5fe6b72c5
Obed <drob.dev@gmail.com>
2021-10-16 00:55:21 -0500

    Add GetParams

--- utils/jquery.utils2.js
+++ utils/jquery.utils2.js
@@ -23,6 +23,61 @@ $.fn.validate = function () {
     }
 }
 /**
+ * getParams
+ * @param {bool} actions 
+ * @param {bool} pswdEncrypt 
+ * @returns 
+ */
+$.fn.getParams = function (actions, pswdEncrypt) {
+    pswdEncrypt = typeof pswdEncrypt !== 'undefined' ? pswdEncrypt : false;
+
+    if (this.prop('tagName') === 'FORM') {
+        data = {
+            params: {},
+            options: {},
+            outside: false
+        };
+
+        if (actions) {
+            data.options['action'] = this.attr('data-action') != undefined ? this.attr('data-action') : null;
+            data.options['url'] = this.attr('action') != undefined ? this.attr('action') : null;
+        }
+
+
+        $.each(this.find('input, select, textarea'), function () {
+            if ($(this).val() !== '' && $(this).attr('name') != undefined) {
+                if ($(this).attr('data-form') !== 'not') {
+                    if ($(this).attr('type') == 'radio') {
+                        if ($(this).is(':checked')) {
+                            data.params[$(this).attr('name')] = ($(this).attr('data-option') == "on" || $(this).attr('data-option') == '1') ? true : false;
+                        }
+                    } else if ($(this).attr('type') == 'password') {
+                        var pswd = $(this).val();
+
+                        if (pswdEncrypt)
+                            pswd = sha512(pswd);
+
+                        data.params[$(this).attr('name')] = pswd;
+                    } else if ($(this).attr('type') == 'hidden') {
+                        data.options[$(this).attr('name')] = $(this).val();
+                    } else if ($(this).attr('type') == 'file') {
+                        data[$(this).attr('name')] = $(this)[0].files[0];
+                    } else if ($(this).attr('type') == 'button') {
+
+                    } else {
+                        data.params[$(this).attr('name')] = $(this).val();
+                    }
+                }
+            }
+        });
+
+        return data;
+    } else {
+        console.log(this.prop('tagName') + ' is not Form')
+        return false;
+    }
+};
+/**
  * 
  * @param {Object} params 
  */
